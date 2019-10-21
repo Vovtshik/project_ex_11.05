bcdb64b457d7f486da3563aee4d99577de511df2
Volodymir Konovalov <vvkon@meta.ua>
2019-10-21 06:37:30 +0600

    Update SurceE1105.cpp

--- SurceE1105.cpp
+++ SurceE1105.cpp
@@ -1,9 +1,14 @@
 #include "../std_lib_facilities.h"
 
-void tolower(string& s);
+void tolower_s(string& s);
 
 int main()
 {
 
     return 0;
+}
+
+void tolower_s(string& s)
+{
+    for(char& x: s) x = tolower(x);
 }
