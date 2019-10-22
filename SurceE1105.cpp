1c404f7780d2382a225cb268afec4db501324a1a
Volodymir Konovalov <vvkon@meta.ua>
2019-10-22 06:53:17 +0300

    Update SurceE1105.cpp

--- SurceE1105.cpp
+++ SurceE1105.cpp
@@ -17,8 +17,40 @@ void tolower_s(string& s)
 void symbol_category(string& s)
 {
     istringstream is(s);
+    char temp;
     for(char ch; is.get(ch);)
     {
-        if()
+        if(isspace(ch)) {
+            cout << "\'"<< ch << "\'" << " - whitespace \n";
+            temp = ch;
+        }
+        else if(isdigit(ch)) {
+            cout << "\'"<< ch << "\'" << " - numeric character \n";
+            temp = ch;
+        }
+        else if(isalpha(ch)) 
+        {
+            if(ch == 'x') 
+            {
+                char ch2;
+                is >> ch2;
+                if(isdigit(temp) && isdigit(ch2)) 
+                {
+                    cout << "\'"<< ch << "\'" << " - arithmetic operations character \n";
+                    is.unget();
+                    temp = ch;
+                }
+            }
+            else
+            {
+                cout << "\'"<< ch << "\'" << " - letter character \n";
+                temp = ch;
+            }
+            
+        }
+        else if( ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            cout << "\'"<< ch << "\'" << " - arithmetic operations character \n";
+            temp = ch;
+        }
     }
 }
