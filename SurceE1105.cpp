6b853fe034e844703b9be891ece431f362b545ca
Volodymir Konovalov <vvkon@meta.ua>
2019-10-22 20:05:38 +0300

    Update SurceE1105.cpp

--- SurceE1105.cpp
+++ SurceE1105.cpp
@@ -2,7 +2,6 @@
 
 #include "../std_lib_facilities.h"
 
-void tolower_s(string& s);
 void symbol_category(string& s);
 
 int main()
@@ -24,11 +23,6 @@ int main()
             symbol_category(str);
     }
     return 0;
-}
-
-void tolower_s(string& s)
-{
-    for(char& x: s) x = tolower(x);
 }
 
 void symbol_category(string& s)
